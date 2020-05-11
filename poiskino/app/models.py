87c3e45a18d4b416984bafb5c10cfae984277c23
Polyakova Ksenya <KsenyaPolyakovaaa@mail.ru>
2020-05-11 20:37:32 +0300

    corrected mistakes in models.py

--- poiskino/app/models.py
+++ poiskino/app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class User(models.Model):
-	id = models.AutoField(primary_key = True)
 	login = models.CharField(max_length = 50, unique = True,
 		verbose_name = "логин")
 	password = models.CharField(max_length = 60, 
@@ -14,8 +13,28 @@ class User(models.Model):
 		print("User is saved!")
 		super(User, self).save(*args, **kwargs)
 
+class Actor(models.Model):
+	name = models.CharField(max_length = 150, unique = True)
+
+	def __str__(self):
+		return self.name
+
+	def save(self, *args, **kwargs):
+		print("Actor is saved!")
+		super(Actor, self).save(*args, **kwargs)
+
+class Director(models.Model):
+	name = models.CharField(max_length = 150, unique = True)
+
+	def __str__(self):
+		return self.name
+
+	def save(self, *args, **kwargs):
+		print("Director is saved!")
+		super(Director, self).save(*args, **kwargs)
+
 class Film(models.Model):
-		GENRE = (
+	GENRE = (
 	(1, "Боевик"),
 	(2, "Вестерн"),
 	(3, "Детектив"),
@@ -28,13 +47,13 @@ class Film(models.Model):
 	(10, "Триллер"),
 	(11, "Фантастика")
 	)
-	id = models.AutoField(primary_key = True)
+
 	name = models.CharField(max_length = 100, verbose_name = "название")
 	description = models.TextField(verbose_name = "описание")
 	country = models.CharField(max_length = 100, 
 		verbose_name = "страна")
 	year = models.PositiveSmallIntegerField(verbose_name = "год")
-	genre = models.IntegerField(choice = GENRE, verbose_name = "жанр")
+	genre = models.IntegerField(choices = GENRE, verbose_name = "жанр")
 	actors = models.ManyToManyField(Actor, verbose_name = "актеры")
 	directors = models.ManyToManyField(Director, 
 		verbose_name = "режессеры")
@@ -47,7 +66,6 @@ class Film(models.Model):
 		super(Film, self).save(*args, **kwargs)
 
 class Like(models.Model):
-	id = models.AutoField(primary_key = True)
 	user_id = models.ForeignKey(User, on_delete = models.PROTECT)
 	film_id = models.ForeignKey(Film, on_delete = models.PROTECT)
 
@@ -58,34 +76,11 @@ class Like(models.Model):
 		print("Like is saved!")
 		super(Like, self).save(*args, **kwargs)
 
-class Actor(models.Model):
-	id = models.AutoField(primary_key = True)
-	name = models.CharField(max_length = 150, unique = True)
-
-	def __str__(self):
-		return self.name
-
-	def save(self, *args, **kwargs):
-		print("Actor is saved!")
-		super(Actor, self).save(*args, **kwargs)
-
-class Director(models.Model):
-	id = models.AutoField(primary_key = True)
-	name = models.CharField(max_length = 150, unique = True)
-
-	def __str__(self):
-		return self.name
-
-	def save(self, *args, **kwargs):
-		print("Director is saved!")
-		super(Director, self).save(*args, **kwargs)
-
 class List(models.Model):
-	id = models.AutoField(primary_key = True)
 	user_id = models.ForeignKey(User, on_delete = models.PROTECT)
 	name = models.CharField(max_length = 150, unique = True, 
 		verbose_name = "название")
-	films = ManyToManyField(Film, verbose_name = "фильмы")
+	films = models.ManyToManyField(Film, verbose_name = "фильмы")
 
 	def __str__(self):
 		return self.name
