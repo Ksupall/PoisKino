bf6f13418e98985b9ecbb1974ac1aa0ee4d07737
Polyakova Ksenya <KsenyaPolyakovaaa@mail.ru>
2020-05-22 21:08:33 +0300

    added function index

--- poiskino/app/views.py
+++ poiskino/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from app.models import Film, Actor
 
-# Create your views here.
+def index(request):
+	films = Film.objects.filter(year=2020)
+	return render(request, "index.html", {"films": films})
+
+def detail(request, film_id):
+	try:
+		film = Film.objects.get(pk = film_id)
+	except Film.DoesNotExist:
+		raise Http404
+	s = "film.id " + str(film.id) + "<br><br>" +" - film.name" + film.name
+	return HttpResponse(s)
