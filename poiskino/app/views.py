dcc6b98cb13e3f990bb0a7d6a046d2b940b05f08
Polyakova Ksenya <KsenyaPolyakovaaa@mail.ru>
2020-05-22 22:54:11 +0300

    added about part

--- poiskino/app/views.py
+++ poiskino/app/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse, Http404
 from app.models import Film, Actor
 
 def index(request):
-	films = Film.objects.filter(year=2020)
-	return render(request, "index.html", {"films": films})
+	#films = Film.objects.filter(year=2020)
+	films = Film.objects.all()
+	return render(request, "news.html", {"films": films})
 
 def detail(request, film_id):
 	try:
@@ -13,3 +14,6 @@ def detail(request, film_id):
 		raise Http404
 	s = "film.id " + str(film.id) + "<br><br>" +" - film.name" + film.name
 	return HttpResponse(s)
+
+def about(request):
+	return render(request, "about.html", {})
