ad5a57ffa2d668a076d979fb4df1f254f4c5bc8d
Polyakova Ksenya <KsenyaPolyakovaaa@mail.ru>
2020-05-22 21:09:41 +0300

    file is edited

--- poiskino/app/admin.py
+++ poiskino/app/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
 from app.models import User, Actor, Director, List, Film, Like
 
-admin.site.register(User)
-admin.site.register(Actor)
-admin.site.register(Director)
-admin.site.register(List)
-admin.site.register(Film)
-admin.site.register(Like)
+admin.site.register([User, Actor, Director, List, Film, Like])
